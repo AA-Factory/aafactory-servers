96b358a77f51e5065226d1261679c17990aa63fa
Jeremy de Gail <jeremy.degail@hoganlovells.com>
2025-09-26 12:29:13 +0200

    [Feature] Create qwen image server

---
+++ qwen_image/celery_worker.py
@@ -0,0 +1,20 @@
+from processing.generate_image import run_text_to_image
+
+from celery import Celery
+import os
+
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = os.getenv("REDIS_PORT", 6379)
+
+app = Celery(
+    "qwen_image_worker",
+    broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
+    backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
+)
+
+
+@app.task(name="text_to_image", queue="qwen_image")
+def text_to_image(positive_prompt: str, negative_prompt: str, image_ratio: str) -> str:
+    result = run_text_to_image(positive_prompt=positive_prompt, negative_prompt=negative_prompt, image_ratio=image_ratio)
+    return result.decode('utf-8')
