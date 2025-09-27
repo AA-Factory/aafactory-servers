f8dca9d05956638532fa66836f9e8f9959a92ebc
Reekomer <jeremy.degail@gmail.com>
2025-09-27 22:45:11 +0200

    [Feature] Add Qwen Edit

--- qwen_image/celery_worker.py
+++ qwen_image/celery_worker.py
@@ -1,4 +1,4 @@
-from processing.generate_image import run_text_to_image
+from processing.generate_image import run_text_to_image, run_image_to_image_edit
 
 from celery import Celery
 import os
@@ -16,5 +16,11 @@ app = Celery(
 
 @app.task(name="text_to_image", queue="qwen_image")
 def text_to_image(positive_prompt: str, negative_prompt: str, image_ratio: str, image_quality: str) -> str:
-    result = run_text_to_image(positive_prompt=positive_prompt, negative_prompt=negative_prompt, image_ratio=image_ratio)
+    result = run_text_to_image(positive_prompt=positive_prompt, negative_prompt=negative_prompt, image_ratio=image_ratio, image_quality=image_quality)
+    return result.decode('utf-8')
+
+
+@app.task(name="image_to_image_edit", queue="qwen_image")
+def image_to_image_edit(image_bytes: str, positive_prompt: str, negative_prompt: str, image_quality: str) -> str:
+    result = run_image_to_image_edit(image_bytes=image_bytes, positive_prompt=positive_prompt, negative_prompt=negative_prompt, image_quality=image_quality)
     return result.decode('utf-8')
