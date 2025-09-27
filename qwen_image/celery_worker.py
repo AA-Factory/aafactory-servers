d561fd284217bb9c0a2cf53e597af3dd8f304450
Reekomer <jeremy.degail@gmail.com>
2025-09-27 21:07:11 +0200

    [Fix] Image quality parameter is missing

--- qwen_image/celery_worker.py
+++ qwen_image/celery_worker.py
@@ -15,6 +15,6 @@ app = Celery(
 
 
 @app.task(name="text_to_image", queue="qwen_image")
-def text_to_image(positive_prompt: str, negative_prompt: str, image_ratio: str) -> str:
+def text_to_image(positive_prompt: str, negative_prompt: str, image_ratio: str, image_quality: str) -> str:
     result = run_text_to_image(positive_prompt=positive_prompt, negative_prompt=negative_prompt, image_ratio=image_ratio)
     return result.decode('utf-8')
