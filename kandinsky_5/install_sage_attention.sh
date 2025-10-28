ddc0cd1210206006b0495e0e7dd2d6d5e7c506cd
Reekomer <jeremy.degail@gmail.com>
2025-10-29 00:18:56 +0100

    [Feature] Add Kadinsky server

---
+++ kandinsky_5/install_sage_attention.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+git clone https://github.com/thu-ml/SageAttention.git
+cd SageAttention
+git reset --hard 68de379
+
+export EXT_PARALLEL=4
+export NVCC_APPEND_FLAGS="--threads 8"
+export MAX_JOBS=32
+
+python setup.py install
---
+++ kandinsky_5/kandinsky/download_models.py
@@ -0,0 +1,74 @@
+import os
+
+from huggingface_hub import snapshot_download
+
+
+if __name__ == "__main__":
+
+    cache_dir = "./weights"
+    
+    dit_path = snapshot_download(
+        repo_id="ai-forever/Kandinsky-5.0-T2V-Lite-pretrain-5s",
+        allow_patterns="model/*",
+        local_dir=cache_dir,
+    )
+
+    dit_path = snapshot_download(
+        repo_id="ai-forever/Kandinsky-5.0-T2V-Lite-pretrain-10s",
+        allow_patterns="model/*",
+        local_dir=cache_dir,
+    )
+
+    dit_path = snapshot_download(
+        repo_id="ai-forever/Kandinsky-5.0-T2V-Lite-sft-5s",
+        allow_patterns="model/*",
+        local_dir=cache_dir,
+    )
+
+    dit_path = snapshot_download(
+        repo_id="ai-forever/Kandinsky-5.0-T2V-Lite-sft-10s",
+        allow_patterns="model/*",
+        local_dir=cache_dir,
+    )
+
+    dit_path = snapshot_download(
+        repo_id="ai-forever/Kandinsky-5.0-T2V-Lite-nocfg-5s",
+        allow_patterns="model/*",
+        local_dir=cache_dir,
+    )
+
+    dit_path = snapshot_download(
+        repo_id="ai-forever/Kandinsky-5.0-T2V-Lite-nocfg-10s",
+        allow_patterns="model/*",
+        local_dir=cache_dir,
+    )
+
+    dit_path = snapshot_download(
+        repo_id="ai-forever/Kandinsky-5.0-T2V-Lite-distilled16steps-5s",
+        allow_patterns="model/*",
+        local_dir=cache_dir,
+    )
+
+    dit_path = snapshot_download(
+        repo_id="ai-forever/Kandinsky-5.0-T2V-Lite-distilled16steps-10s",
+        allow_patterns="model/*",
+        local_dir=cache_dir,
+    )
+    
+    vae_path = snapshot_download(
+        repo_id="hunyuanvideo-community/HunyuanVideo",
+        allow_patterns="vae/*",
+        local_dir=cache_dir,
+    )
+
+    text_encoder_path = snapshot_download(
+        repo_id="Qwen/Qwen2.5-VL-7B-Instruct",
+        local_dir=os.path.join(cache_dir, "text_encoder/"),
+    )
+
+    text_encoder2_path = snapshot_download(
+        repo_id="openai/clip-vit-large-patch14",
+        local_dir=os.path.join(cache_dir, "text_encoder2/"),
+    )
+
+    
